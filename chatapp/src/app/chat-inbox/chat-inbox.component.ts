3c5dd7a2cc4f25d548c70caa5c122167001c408b
MadhusudanGT <madhusudangt97@gmail.com>
2020-11-03 13:36:08 +0530

    commit

--- chatapp/src/app/chat-inbox/chat-inbox.component.ts
+++ chatapp/src/app/chat-inbox/chat-inbox.component.ts
@@ -13,15 +13,21 @@ const SOCKET_ENDPOINT = 'localhost:3000';
 export class ChatInboxComponent implements OnInit {
   title = 'Websocket Angular client ';
   userName: string;
+  password:any;
   message: string;
   output: any[] = [];
   feedback: string;
+  userexists:string;
+  userexistlist:any[]=[];
   public show:boolean = false;
   public reg:boolean = true;
 
   constructor(private webSocketService:ChatService) { }
   ngOnInit(): void {
-    
+    this.webSocketService.listen('typing').subscribe((data) => this.updateFeedback(data));
+    this.webSocketService.listen('chat').subscribe((data) => this.updateMessage(data));
+    this.webSocketService.listen('userExists').subscribe((data)=>this.userexist(data))
+    this.webSocketService.listen('userSet').subscribe((data)=>this.currentuserlist(data))
   }
 
   messageTyping(): void {
@@ -48,9 +54,23 @@ export class ChatInboxComponent implements OnInit {
   }
 
   registerMessage(){
-    this.webSocketService.listen('typing').subscribe((data) => this.updateFeedback(data));
-    this.webSocketService.listen('chat').subscribe((data) => this.updateMessage(data));
+    //************** */
+    this.webSocketService.SetUserName(this.userName,this.password);
+    //************** */
+
     this.show=true;
     this.reg=false;
+  }
+
+  userexist(data:any){
+this.userexists=  `${data} is alredy exist please chose onther name`
+this.reg=true;
+this.show=false;
+  }
+
+  currentuserlist(data){
+    console.log(data)
+this.userexistlist=data;
+
   }
 }
--- chatapp/src/app/chat.service.ts
+++ chatapp/src/app/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
 import {Observable } from 'rxjs';
+import { ReturnStatement } from '@angular/compiler';
 
 
 @Injectable({
@@ -24,4 +25,28 @@ export class ChatService {
     emit(eventname: string, data: any) {
         this.socket.emit(eventname, data);
     }
+
+SetUserName(username:string,password:any){
+    var loginDetails={ 
+        username : username,
+        password : password 
+        };
+        console.log(loginDetails)
+    this.socket.emit('setUsername', loginDetails);
+
+    this.socket.on('userExists', function(data) {
+        return "choose onther name user alredy exist"+data;
+     });
+
+     this.socket.on('userExists', function(data){
+         console.log(`${data.username} is alredy exist`)
+         return data;
+     })
+     this.socket.on('userSet', function(data){
+        
+        return data;
+    } )
+}
+
+
 }
